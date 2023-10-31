cd49f769df7e3cf06e14eefbc7625ff8357266e6
Luca Goddijn <lgoddijn@f0r2s7.codam.nl>
2023-10-31 17:10:13 +0100

    first documentation out of a billion more to come :')

--- math/ft_sqrtf.c
+++ math/ft_sqrtf.c
@@ -6,11 +6,64 @@
 /*   By: lgoddijn <lgoddijn@student.codam.nl >      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 16:15:04 by lgoddijn          #+#    #+#             */
-/*   Updated: 2023/10/26 18:31:30 by lgoddijn         ###   ########.fr       */
+/*   Updated: 2023/10/31 17:09:48 by lgoddijn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
+
+/*
+
+@def
+	`math/ft_sqrtf.c : ft_sqrtf`
+
+@brief
+	Calculates the square root of a float number.
+
+@description
+	Calculates the square root of a float
+	number using the quake algorithm.
+
+@synopsis
+	`float	ft_sqrtf(float n);`
+
+@params
+	`float n` The number of which the sqrt is calculated.
+
+@return
+	`float` The square root of `n`
+
+@see
+	`libft.h : u_sqrtf`
+
+@notes
+	The function uses a union named `u_sqrtf`.
+	A union allows one to store different data types
+	in the same memory location. The union is used 
+	to perform type punning, which allows us to manipulate
+	the bitwise representation of a float as an integer.
+
+    The function sets the float member of the union to the input value, `n`.
+
+    `conv.i = 0x5fe6eb50c7b537a9 - (conv.i >> 1);`:
+	This is the heart of the fast inverse square root method.
+	The number `0x5fe6eb50c7b537a9` is a "magic" constant.
+	By performing this operation on the integer representation of a float,
+	we're making an initial approximation of the inverse square root.
+	The bit-shifting operation `(>> 1)` roughly divides the exponent of
+	the floating-point number by 2, which is an approximation of the square root.
+
+	`conv.d *= (th - (x2 * conv.d * conv.d));`:
+	This line refines the approximation. It uses the Newton-Raphson method,
+	which is a numerical method to find successively better approximations
+	to the roots of a real-valued function. Here, it's applied to refine
+	our initial guess of the inverse square root.
+
+	To have a more precise return use `math/ft_sqrtd.c : ft_sqrtd`.
+
+	The function returns the square root of `n` by taking
+	the inverse of our computed inverse square root.
+*/
 
 float	ft_sqrtf(float n)
 {
