139a7ab306103904e417f44358c8a49aa5b803d2
Luca Goddijn <lgoddijn@f0r3s9.codam.nl>
2023-11-27 16:36:20 +0100

    oops there was a wrong bracket thingy

--- stack/ft_pop_stack.c
+++ stack/ft_pop_stack.c
@@ -6,7 +6,7 @@
 /*   By: lgoddijn <lgoddijn@student.codam.nl >      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/25 16:30:09 by lgoddijn          #+#    #+#             */
-/*   Updated: 2023/11/25 17:59:43 by lgoddijn         ###   ########.fr       */
+/*   Updated: 2023/11/27 16:35:19 by lgoddijn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ bool	ft_pop_stack(t_stack *stack, void *out)
 	if (!stack || ft_is_empty_stack(stack))
 		return (false);
 	ft_memcpy(out, stack->content + \
-	(stack->top * stack->elem_size, \
-	stack->elem_size), stack->elem_size);
+	(stack->top * stack->elem_size), \
+	stack->elem_size);
 	--(stack->top);
 	return (true);
 }
