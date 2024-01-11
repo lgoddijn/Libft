7b11a81906541a18fe07432061dddd162a023b3e
Luca Goddijn <lgoddijn@f0r3s6.codam.nl>
2024-01-11 17:27:22 +0100

    added a 2d array freeing function

--- include/ft_heap.h
+++ include/ft_heap.h
@@ -6,7 +6,7 @@
 /*   By: lgoddijn <lgoddijn@student.codam.nl >      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/31 15:31:33 by lgoddijn          #+#    #+#             */
-/*   Updated: 2024/01/11 15:39:20 by lgoddijn         ###   ########.fr       */
+/*   Updated: 2024/01/11 17:24:34 by lgoddijn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,5 +28,6 @@
 void	*ft_mmap(size_t __size, int __prot, int __flags, off_t __offset);
 void	*ft_calloc(size_t nmemb, size_t size);
 void	*ft_realloc(void *ptr, size_t size);
+void	ft_free2d(void **ptr);
 
 #endif
