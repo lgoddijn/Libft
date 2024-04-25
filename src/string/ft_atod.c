da14e428eb6f0ed24e125a72349b606ae0b3353c
Luca Goddijn <lgoddijn@f1r2s3.codam.nl>
2024-04-25 16:36:01 +0200

    fixed atod whooops

--- src/string/ft_atod.c
+++ src/string/ft_atod.c
@@ -6,7 +6,7 @@
 /*   By: lgoddijn <lgoddijn@student.codam.nl >      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 17:47:41 by lgoddijn          #+#    #+#             */
-/*   Updated: 2024/04/14 18:27:42 by lgoddijn         ###   ########.fr       */
+/*   Updated: 2024/04/25 16:33:26 by lgoddijn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,17 @@
 
 static int32_t	skip_space_sign(const char *s, int32_t *is_neg)
 {
-	int32_t	i;
+	int32_t i;
 
 	i = 0;
-	while (ft_isspace(s[i++]))
-		;
-	if ((s[i] == '+' || s[i] == '-') && s[i++] == '-')
-		*is_neg *= -1;
+	while (ft_isspace((unsigned char)s[i]))
+		++i;
+	if (s[i] == '-' || s[i] == '+')
+	{
+		if (s[i] == '-')
+			*is_neg = -1;
+		++i;
+	}
 	return (i);
 }
 
@@ -33,22 +37,19 @@ double	ft_atod(const char *s)
 
 	if (!s)
 		return ((double)K0_C_LMA0);
-	n = 0;
-	div = 0.1;
 	is_neg = 1;
+	div = 0.1;
+	n = 0.0;
 	i = skip_space_sign(s, &is_neg);
-	if (s[i] && !ft_isdigit(s[i]))
-		return ((double)K0_C_LMA0);
-	--i;
-	while (s[++i] && ft_isdigit(s[i]) && s[i] != '.')
-		n = (n * 10.0) + (s[i++] - '0');
-	if (s[i] == '.')
-		++i;
 	while (s[i] && ft_isdigit(s[i]))
+		n = n * 10 + (s[i++] - '0');
+	if (s[i] == '.')
 	{
-		n = n + ((s[i] - '0') * div);
-		div *= 0.1;
-		++i;
+		while (s[++i] && ft_isdigit(s[i]))
+		{
+			n += (s[i] - '0') * div;
+			div *= 0.1;
+		}
 	}
 	return (n * is_neg);
 }
